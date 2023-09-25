71be944ea1f1836b5172128d3a3c2fdef60f5428
Chris Field <94464835+crfield18@users.noreply.github.com>
2023-09-25 11:38:36 +0100

    trying number input mobile keyboard

--- scripts/protein_conc.js
+++ scripts/protein_conc.js
@@ -12,7 +12,7 @@ const resultParagraph = document.getElementById("result");
 // Function to perform the calculation
 function calculateSum() {
     // Absorbance of the sample at a specific wavelength
-    // (typically 280 nm for estimating protein concentration  
+    // (typically 280 nm for estimating protein concentration
     const num1 = new Big(AbsInput.value);
     // Dilution factor
     // By how much was the sample diluted to allow for accurate readings
@@ -29,14 +29,16 @@ function calculateSum() {
     const MicroMolar = (num1 * num2) * 1000000 / (num3 * num4);
     const RoundedMicroMolar = MicroMolar.toFixed(2);
 
+    console.log(`conc in um: ${MicroMolar}`);
+
     // Is the final concentration valid (is it a positive number)
-    if (isNaN(RoundedMicroMolar) || RoundedMicroMolar <= 0) {
+    if (Number.isNaN(RoundedMicroMolar) || RoundedMicroMolar <= 0) {
         resultParagraph.textContent = `Result is not valid (RoundedMicroMolar\
             = ${RoundedMicroMolar})`;
         return;
     }
-    // Only print concentration in µm if protein mass is not set or is <= 0 
-    if (isNaN(num5) || num5 <= 0) {
+    // Only Print concentration in mg/ml if protein mass is a number above 0
+    if (Number.isNaN(num5) || num5 <= 0) {
         resultParagraph.textContent = `Estimated protein concentration:\
             ${RoundedMicroMolar} µM`;
         return;
@@ -44,14 +46,10 @@ function calculateSum() {
         const mgml = (MicroMolar / 1000000) * num5;
         resultParagraph.textContent = `Estimated protein concentration:\
             ${RoundedMicroMolar} µM or ${mgml.toFixed(2)} mg/ml`;
+            console.log(`conc in mg/ml: ${mgml}`);
         return;
     }
-    
-    // Think about adding a method for shifting between M mM µM
-    // depending on the size of RoundedMicroMolar
 
-    // Maybe also show the concentration in M in brackets to like 10 d.p.
-    // as a backup
 }
 
 // Add click event listener to the button
